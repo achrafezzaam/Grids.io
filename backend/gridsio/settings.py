05a005985ce498a6b4e3a7af01b4dd1e2c78ff66
achrafezzaam <achraf.ezzaam@gmail.com>
2023-12-05 11:18:46 +0100

    Connecting the backend with the front end

--- backend/gridsio/settings.py
+++ backend/gridsio/settings.py
@@ -39,11 +39,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'gridsio',
     'rest_framework',
+    'corsheaders',
 ]
+
+CORS_ALLOWED_HEADERS = ['http://localhost:5173']
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
